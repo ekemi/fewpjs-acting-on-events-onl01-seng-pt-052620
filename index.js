8e1fe3127a9d4a03928df4d7e55a9bfae8ee5602
edmundo papi <edmundopierre@yahoo.com>
2020-11-25 07:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px","");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 10) {
+    dodger.style.left = `${left-1}px`;
+  }
+}
+dodger.addEventListener("keydown",function(e){
+  if (e.key =="ArrowLeft"){
+    moveDodgerLeft();
+  }
+});
