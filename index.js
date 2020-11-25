240cd207ac5f0df20e45a83241022bfec6c78a2a
edmundo papi <edmundopierre@yahoo.com>
2020-11-25 07:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-// Your code here
-let dodger = document.getElementById("dodger");
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px","");
-  let left = parseInt(leftNumbers, 10);
-  if (left > 10) {
-    dodger.style.left = `${left-1}px`;
-  }
-}
-dodger.addEventListener("keydown",function(e){
-  if (e.key =="ArrowLeft"){
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
     moveDodgerLeft();
   }
 });
 
-function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px","");
-  let right = parseInt(rightNumbers, 10);
-  if (right > 10) {
-    dodger.style.right = `${left+1}px`;
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
 }
-dodger.addEventListener("keydown",function(e){
-  if (e.key =="ArrowRight"){
-    moveDodgerRight();
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 });
